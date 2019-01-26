a783ecf4b96e813e463a1ccce7e97c17a070c7d8
visualstudio <visualstudio@122-PC38>
2019-01-25 21:22:11 -0500

    gfg

--- c0712759/Program.cs
+++ c0712759/Program.cs
@@ -4,12 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0712759
+namespace LinkedLists
 {
     class Program
     {
         static void Main(string[] args)
         {
         }
+    }
+
+    class Countryside
+    {
+        Village Alst;
+        Village Maeland;
+        Village Schenig;
+
+        public void InitializeMap()
+        {
+            Alst = new Village();
+            Alst.VillageName = "Alst";
+            Maeland = new Village();
+            Maeland.VillageName = "Maeland";
+            Maeland.isAstrildeHere = true;
+            Schenig = new Village();
+            Schenig.VillageName = "Schenig";
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+
+        public void FindAstrilde()
+        {
+
+            Village CurrentVillage = this.Maeland;
+            if (CurrentVillage.isAstrildeHere)
+            {
+                Console.WriteLine("Hugi found Astrilde!!! Celebrations! XOXO");
+            }
+            Village NextVillage;
+
+            NextVillage = CurrentVillage.NextVillage;
+
+        }
+    }
+
+    class Village
+    {
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+        public Village() { }
     }
 }
